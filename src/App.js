621a37c077a74a2a9a317009f0e25cff1bb4827d
Dhruvermafz <vermadhruv09112002@gmail.com>
2022-11-19 13:39:49 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav/Nav';
-
+import Header from './components/header/Header';
+import Content from './components/content/Content';
 
 const App = () =>{
   return (
     <div>
        <Nav />
+       <Header/>
+       <Content/>
     </div>
   )
 }
